f41f2b6a7f640acac95f472fdabf53ff47327ff5
Natalia <147244357+NataliaTokhadze@users.noreply.github.com>
2023-12-16 23:02:56 +0400

    Add files via upload

---
+++ You are my liberty.py
@@ -0,0 +1,256 @@
+#1
+
+class Rectangle :
+    def __init__ (self, width, length):
+        self.width = width
+        self.length = length 
+    def perimetri (self) :
+        p = 2*(self.width + self.length)
+        return p
+    def fartobi(self) :
+        s = self.width * self.length
+        return s
+
+obj = Rectangle(1,5)
+
+print(obj.perimetri())
+print(obj.fartobi())
+
+
+
+#2
+
+class Car :
+    def __init__ (self, color, model, makeyear) :
+        self.color = color 
+        self.model = model
+        self.makeyear = makeyear
+    def buy(self) :
+        return f"You buy {self.color} {self.model} {self.makeyear}"
+    def sell(self) :
+        return f"You sell {self.color} {self.model} {self.makeyear}"
+    def rent(self) :
+        return "I don't know what means rent and ai do noot cnow ynglissh, ssori"
+    
+obj1 = Car ("red", "mercedes", 2015)
+obj2 = Car ("cat", "meeeeeeeeeeeeooooooooow", "why cat cann't car?")
+obj3 = Car ("any", "any", "any")
+
+print(obj1.buy())
+print(obj1.sell())
+print(obj1.rent())
+
+print(obj2.buy())
+print(obj2.sell())
+print(obj2.rent())
+
+print(obj3.buy())
+print(obj3.sell())
+print(obj3.rent())
+
+
+#3
+
+class Dog() :
+    def __init__ (self, breed, size, age, color) :
+        self.breed = breed
+        self.size = size
+        self.age = age
+        self.color = color
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+# martla gegonat, rom dzagzle davwer kods??
+# AM MSOFLIOS DAIPYROBS KATA DA ARA DZAGLI!!!!
+
+class Cat() :
+    def __init__ (self, breed, size, age, color) :
+        self.breed = breed
+        self.size = size
+        self.age = age
+        self.color = color
+    def worship(self):
+        return f"You worhip cat and don't matter what kind of cat! >:3"
+    
+cat1 = Cat(" /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ")
+cat2 = Cat(" /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ")
+cat3 = Cat(" /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ")
+cat4 = Cat(" /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ")
+cat5 = Cat(" /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ")
+# UFRO BEVRI KATA!!!!!!
+cat6 = Cat(" /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ")
+cat7 = Cat(" /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ")
+cat8 = Cat(" /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ")
+cat9 = Cat(" /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ")
+cat10 = Cat(" /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ", " /⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\ ")
+
+print(cat1.worship())
+print(cat2.worship())
+print(cat4.worship())
+print(cat4.worship())
+print(cat5.worship())
+print(cat6.worship())
+print(cat7.worship())
+print(cat8.worship())
+print(cat9.worship())
+print(cat10.worship())
+
+
+#4
+
+class Math() :
+    def __init__ (self, number1 , number2) :
+        self.number1 = number1
+        self.number2 = number2
+    def add(self) :
+        return self.number1 * self.number2
+    def divide(self) :
+        return self.number1 + self.number2
+    def subtract(self) :
+        return self.number1 / self.number2
+    def multiply(self) :
+        return self.number1 - self.number2
+    
+
+nums = Math(10000000000000000000000000000000000000000000000000000000, 1000000000000000000000000000)
+
+print(nums.add())
+print(nums.divide())
+print(nums.subtract())
+print(nums.multiply())
+
+# Xom swori pasuxi miiget? 
+# MUAHAHAHAHAHA 
+# CHEM BOROTEBAS SAZGVARI AR ARQVS!!!
+# au magrad mecineba raaaa XD
+
+
+
+#5
+
+# exlaxans vuyurebdi ambebs da politikur mdgomareobas
+# da radgan tqven ar daazustet metodebi... >:D
+
+
+class People:
+    def __init__(self, person1, person2, person3) :
+        self.person1 = person1
+        self.person2 = person2
+        self.person3 = person3
+    def give_rose(self):
+        return f"{self.person1}: IT'S A REVOLUTION!!! GO AWAY SHEVARDNADZE!!!"
+    def archevnebis_gayalbeba(self):
+        return f"{self.person2}: au misha shechema ra ginda???"
+    def fexburteli(self):
+        return f"{self.person3}: ragac merobis mugamze var..."
+
+
+class Student(People) :
+    def give_food(self, student):
+        return f"{self.person2}: {student}, ginda 2 kg kartofili? mashin shemoxaze chemi saxeli"
+    
+class Lecturer(People):
+    def pay_taxes(self):
+        return f"{self.person3}: ra tqma unda me am fuls davxarjav, rom chveni qalaqi ayvavdes da ar daitoveb tavistvis!"
+
+class Saakashvili(People):
+    def give_voice(self):
+        return f"{self.person3}: AM KORUFCIONELEBIS DED...#@$%^@&@#$%^@" #cenzura ._."
+GADADEQI = Student("Saakashvili","Shevardnadze","Kaladze")
+GADADEQI1 = Lecturer("Saakashvili","Shevardnadze","Kaladze")
+print(GADADEQI.give_rose())
+print(GADADEQI.archevnebis_gayalbeba())
+print(GADADEQI.fexburteli())
+print(GADADEQI.give_food("Mariam"))
+print(GADADEQI1.pay_taxes())
+
+
+
+
+
+#BONUS
+
+class Bank_Account:
+    def __init__(self,  account_name, balance = 0):
+        self.account_name = account_name
+        self.balance = balance
+    def get_balance(self):
+        return self.balance
+    def set_balance(self, lari):
+        self.balance = lari
+        return self.balance
+    def deposit(self, money):
+        self.money = money
+        m = self.money + self.balance
+        return f"Tqven charicxet fuli da tqveni angarishi udris: {m} lars"
+    def withdraw(self, cash):
+        self.cash = cash
+        c = self.balance - self.cash
+        return f"Tqven gaqvt agebuli krediti, amitomac tqveni balanci sheadgens: {c} lars"
+        
+        
+b = Bank_Account("Bomji Loti")
+print(b.get_balance())
+print(b.set_balance(0.01))
+print(b.deposit(0.01))
+print(b.withdraw(0.02))
+
+# au medzinebaaaaaa
+# Good night! :3
